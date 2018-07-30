ddcda87a8beba049ff83ea1681d8e142bda9bcec
Jay Lee <jay@j911.me>
2018-07-30 19:01:39 +0900

    add basic structure

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <span>
+            HEllO, VUE, PARSEl AND TYPESCRiPT!
+        </span>
+    </div>
+</template>
+<script lang="ts">
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+  @Component({
+
+  })
+  export default class AppComponent extends Vue {
+      
+  }
+</script>
